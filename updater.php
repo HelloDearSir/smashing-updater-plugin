4786e53e2fcefba3897f8513dbd2c27c87a01a55
HelloDearSir <jonbarton@hotmail.co.uk>
2023-07-15 00:28:16 +0100

    tewestins

--- updater.php
+++ updater.php
@@ -72,6 +72,7 @@ class Smashing_Updater {
 		add_filter( 'upgrader_post_install', array( $this, 'after_install' ), 10, 3 );
 		add_action('woocommerce_breadcrumb','woocommerce_template_loop_product_title',10);
 		add_action('woocommerce_breadcrumb','	woocommerce_result_count ',10);
+		add_action('woocommerce_breadcrumb','	woocommerce_result_count ',1);
 	
 		// Add Authorization Token to download_package
 		add_filter( 'upgrader_pre_download',
