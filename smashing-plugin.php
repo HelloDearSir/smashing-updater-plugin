1bc71571f37e794cccde701827f72d8ab36c5a75
HelloDearSir <jonbarton@hotmail.co.uk>
2025-01-26 23:27:05 +0000

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -5,13 +5,18 @@ Version: 1.2
 Description: Making our own badges and updating product attributes
 
 */
+ 
 
-if(!class_exists('Smashing_updater')) {{
-    include( plugin_dir_path( __FILE__ ) . "updater.php");
-}}
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
 
-$updater = new Smashing_updater(__FILE__);
+$updater = new Smashing_Updater( __FILE__ );
 $updater->set_username('HelloDearSir');
 $updater->set_repository('smashing-updater-plugin');
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
 $updater->initialize();
+
 ?>
